3ad297aadefd04c12682274fcd9a47db8637d792
Satnam Singh <singhkhowal@gmail.com>
2026-09-21 18:37:55 +0530

    Extract PHP page bodies batch 8

---
+++ pages/team-three.php
@@ -0,0 +1,188 @@
+
+        <!--=====================================-->
+        <!--=       Breadcrumb Area Start      =-->
+        <!--=====================================-->
+
+
+        <div class="edu-breadcrumb-area breadcrumb-style-2 bg-image bg-image--19">
+            <div class="container">
+                <div class="breadcrumb-inner">
+                    <div class="page-title">
+                        <h1 class="title">leadership Instruction</h1>
+                    </div>
+                    <ul class="edu-breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item active" aria-current="page">Instructors 3</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!--=====================================-->
+        <!--=        Team Area Start       	=-->
+        <!--=====================================-->
+        <!-- Start Team Area  -->
+        <div class="edu-team-area team-area-3 edu-section-gap">
+            <div class="container">
+                <div class="section-title section-center" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                    <span class="pre-title">Instructors</span>
+                    <h2 class="title">Course Instructors</h2>
+                    <span class="shape-line"><i class="icon-19"></i></span>
+                </div>
+                <div class="row g-5">
+                    <!-- Start Instructor Grid  -->
+                    <div class="col-lg-4 col-md-6" data-sal-delay="100" data-sal="slide-up" data-sal-duration="800">
+                        <div class="edu-team-grid team-style-3">
+                            <div class="inner">
+                                <div class="thumbnail-wrap">
+                                    <div class="thumbnail">
+                                        <a href="team-details.html">
+                                            <img src="assets/images/team/team-05.webp" alt="team images">
+                                        </a>
+                                    </div>
+                                    <ul class="team-share-info">
+                                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                        <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title"><a href="team-details.html">Jane Seymour</a></h5>
+                                    <span class="designation">Teacher</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Instructor Grid  -->
+                    <!-- Start Instructor Grid  -->
+                    <div class="col-lg-4 col-md-6" data-sal-delay="200" data-sal="slide-up" data-sal-duration="800">
+                        <div class="edu-team-grid team-style-3">
+                            <div class="inner">
+                                <div class="thumbnail-wrap">
+                                    <div class="thumbnail">
+                                        <a href="team-details.html">
+                                            <img src="assets/images/team/team-06.webp" alt="team images">
+                                        </a>
+                                    </div>
+                                    <ul class="team-share-info">
+                                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                        <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title"><a href="team-details.html">Edward Norton</a></h5>
+                                    <span class="designation">Teacher</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Instructor Grid  -->
+                    <!-- Start Instructor Grid  -->
+                    <div class="col-lg-4 col-md-6" data-sal-delay="300" data-sal="slide-up" data-sal-duration="800">
+                        <div class="edu-team-grid team-style-3">
+                            <div class="inner">
+                                <div class="thumbnail-wrap">
+                                    <div class="thumbnail">
+                                        <a href="team-details.html">
+                                            <img src="assets/images/team/team-08.webp" alt="team images">
+                                        </a>
+                                    </div>
+                                    <ul class="team-share-info">
+                                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                        <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title"><a href="team-details.html">Penelope Cruz</a></h5>
+                                    <span class="designation">Teacher</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Instructor Grid  -->
+                    <!-- Start Instructor Grid  -->
+                    <div class="col-lg-4 col-md-6" data-sal-delay="300" data-sal="slide-up" data-sal-duration="800">
+                        <div class="edu-team-grid team-style-3">
+                            <div class="inner">
+                                <div class="thumbnail-wrap">
+                                    <div class="thumbnail">
+                                        <a href="team-details.html">
+                                            <img src="assets/images/team/team-16.webp" alt="team images">
+                                        </a>
+                                    </div>
+                                    <ul class="team-share-info">
+                                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                        <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title"><a href="team-details.html">Michael Douglas</a></h5>
+                                    <span class="designation">Teacher</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Instructor Grid  -->
+                    <!-- Start Instructor Grid  -->
+                    <div class="col-lg-4 col-md-6" data-sal-delay="300" data-sal="slide-up" data-sal-duration="800">
+                        <div class="edu-team-grid team-style-3">
+                            <div class="inner">
+                                <div class="thumbnail-wrap">
+                                    <div class="thumbnail">
+                                        <a href="team-details.html">
+                                            <img src="assets/images/team/team-17.webp" alt="team images">
+                                        </a>
+                                    </div>
+                                    <ul class="team-share-info">
+                                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                        <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title"><a href="team-details.html">Hilary Swank</a></h5>
+                                    <span class="designation">Teacher</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Instructor Grid  -->
+                    <!-- Start Instructor Grid  -->
+                    <div class="col-lg-4 col-md-6" data-sal-delay="300" data-sal="slide-up" data-sal-duration="800">
+                        <div class="edu-team-grid team-style-3">
+                            <div class="inner">
+                                <div class="thumbnail-wrap">
+                                    <div class="thumbnail">
+                                        <a href="team-details.html">
+                                            <img src="assets/images/team/team-18.webp" alt="team images">
+                                        </a>
+                                    </div>
+                                    <ul class="team-share-info">
+                                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                        <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title"><a href="team-details.html">Steve Martin</a></h5>
+                                    <span class="designation">Teacher</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Instructor Grid  -->
+                </div>
+            </div>
+        </div>
+        <!-- End Team Area  -->
+        <!--=====================================-->
+        <!--=        Footer Area Start       	=-->
+        <!--=====================================-->
+        <!-- Start Footer Area  -->
+        
---
+++ pages/wishlist.php
@@ -0,0 +1,92 @@
+
+        <!--=====================================-->
+        <!--=       Breadcrumb Area Start      =-->
+        <!--=====================================-->
+
+
+        <div class="edu-breadcrumb-area">
+            <div class="container">
+                <div class="breadcrumb-inner">
+                    <div class="page-title">
+                        <h1 class="title">Wishlist Page</h1>
+                    </div>
+                    <ul class="edu-breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item active" aria-current="page">Wishlist Page</li>
+                    </ul>
+                </div>
+            </div>
+            <ul class="shape-group">
+                <li class="shape-1">
+                    <span></span>
+                </li>
+                <li class="shape-2 scene"><img data-depth="2" src="assets/images/about/shape-13.png" alt="shape"></li>
+                <li class="shape-3 scene"><img data-depth="-2" src="assets/images/about/shape-15.png" alt="shape"></li>
+                <li class="shape-4">
+                    <span></span>
+                </li>
+                <li class="shape-5 scene"><img data-depth="2" src="assets/images/about/shape-07.png" alt="shape"></li>
+            </ul>
+        </div>
+
+        <!--=====================================-->
+        <!--=        Wishlist Area Start        =-->
+        <!--=====================================-->
+        <section class="cart-page-area edu-section-gap">
+            <div class="container">
+                <div class="table-responsive">
+                    <table class="table cart-table wishlist-table">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="product-remove"></th>
+                                <th scope="col" class="product-thumbnail"></th>
+                                <th scope="col" class="product-title">Product Name</th>
+                                <th scope="col" class="product-price">Price</th>
+                                <th scope="col" class="product-status">Stock Status</th>
+                                <th scope="col" class="product-add-cart"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="product-remove">
+                                    <a href="#" class="remove-wishlist"><i class="icon-73"></i></a>
+                                </td>
+                                <td class="product-thumbnail">
+                                    <a href="product-details.html"><img src="assets/images/shop/product-02.jpg" alt="Books"></a>
+                                </td>
+                                <td class="product-title">
+                                    <a href="product-details.html">Natural Science Project</a>
+                                </td>
+                                <td class="product-price" data-title="Price"><span class="currency-symbol">$</span>70.30</td>
+                                <td class="product-status" data-title="Stock"> In Stock</td>
+                                <td class="product-add-cart"><a href="cart.html" class="edu-btn btn-medium">Add to Cart</a></td>
+                            </tr>
+                            <tr>
+                                <td class="product-remove"><a href="#" class="remove-wishlist"><i class="icon-73"></i></a></td>
+                                <td class="product-thumbnail"><a href="product-details.html"><img src="assets/images/shop/product-03.jpg" alt="Digital Product"></a></td>
+                                <td class="product-title"><a href="product-details.html">The King of Drugs</a></td>
+                                <td class="product-price" data-title="Price"><span class="currency-symbol">$</span>70.00</td>
+                                <td class="product-status" data-title="Stock"> In Stock</td>
+                                <td class="product-add-cart"><a href="cart.html" class="edu-btn btn-medium">Add to Cart</a></td>
+                            </tr>
+                            <tr>
+                                <td class="product-remove"><a href="#" class="remove-wishlist"><i class="icon-73"></i></a></td>
+                                <td class="product-thumbnail"><a href="product-details.html"><img src="assets/images/shop/product-06.jpg" alt="Digital Product"></a></td>
+                                <td class="product-title"><a href="product-details.html">The Silver Chair</a></td>
+                                <td class="product-price" data-title="Price"><span class="currency-symbol">$</span>70.00</td>
+                                <td class="product-status" data-title="Stock"> In Stock</td>
+                                <td class="product-add-cart"><a href="cart.html" class="edu-btn btn-medium">Add to Cart</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+        <!--=====================================-->
+        <!--=        Footer Area Start          =-->
+        <!--=====================================-->
+        <!-- Start Footer Area  -->
+        
