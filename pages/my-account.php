04416586fd256bd42e34db6c2b5d9e2d2cf27b4e
Satnam Singh <singhkhowal@gmail.com>
2026-09-21 18:36:55 +0530

    Extract PHP page bodies batch 7

---
+++ pages/my-account.php
@@ -0,0 +1,110 @@
+
+        <!--=====================================-->
+        <!--=       Breadcrumb Area Start      =-->
+        <!--=====================================-->
+
+
+        <div class="edu-breadcrumb-area">
+            <div class="container">
+                <div class="breadcrumb-inner">
+                    <div class="page-title">
+                        <h1 class="title">My Account</h1>
+                    </div>
+                    <ul class="edu-breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item active" aria-current="page">Account</li>
+                    </ul>
+                </div>
+            </div>
+            <ul class="shape-group">
+                <li class="shape-1">
+                    <span></span>
+                </li>
+                <li class="shape-2 scene"><img data-depth="2" src="assets/images/about/shape-13.png" alt="shape"></li>
+                <li class="shape-3 scene"><img data-depth="-2" src="assets/images/about/shape-15.png" alt="shape"></li>
+                <li class="shape-4">
+                    <span></span>
+                </li>
+                <li class="shape-5 scene"><img data-depth="2" src="assets/images/about/shape-07.png" alt="shape"></li>
+            </ul>
+        </div>
+
+        <!--=====================================-->
+        <!--=          Login Area Start         =-->
+        <!--=====================================-->
+        <section class="account-page-area section-gap-equal">
+            <div class="container position-relative">
+                <div class="row g-5 justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="login-form-box">
+                            <h3 class="title">Sign in</h3>
+                            <p>Don’t have an account? <a href="#">Sign up</a></p>
+                            <form>
+                                <div class="form-group">
+                                    <label for="current-log-email">Username or email*</label>
+                                    <input type="email" name="current-log-email" id="current-log-email" placeholder="Email or username">
+                                </div>
+                                <div class="form-group">
+                                    <label for="current-log-password">Password*</label>
+                                    <input type="password" name="current-log-password" id="current-log-password" placeholder="Password">
+                                    <span class="password-show"><i class="icon-76"></i></span>
+                                </div>
+                                <div class="form-group chekbox-area">
+                                    <div class="edu-form-check">
+                                        <input type="checkbox" id="remember-me">
+                                        <label for="remember-me">Remember Me</label>
+                                    </div>
+                                    <a href="#" class="password-reset">Lost your password?</a>
+                                </div>
+                                <div class="form-group">
+                                    <button type="button" class="edu-btn btn-medium">Sign in <i class="icon-4"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="login-form-box registration-form">
+                            <h3 class="title">Registration</h3>
+                            <p>Already have an account? <a href="#">Sign in</a></p>
+                            <form>
+                                <div class="form-group">
+                                    <label for="reg-name">Name*</label>
+                                    <input type="text" name="reg-name" id="reg-name" placeholder="Full name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="log-email">Username or email*</label>
+                                    <input type="email" name="log-email" id="log-email" placeholder="Email or username">
+                                </div>
+                                <div class="form-group">
+                                    <label for="log-password">Password*</label>
+                                    <input type="password" name="log-password" id="log-password" placeholder="Password">
+                                    <span class="password-show"><i class="icon-76"></i></span>
+                                </div>
+                                <div class="form-group chekbox-area">
+                                    <div class="edu-form-check">
+                                        <input type="checkbox" id="terms-condition">
+                                        <label for="terms-condition">I agree the User Agreement and <a href="terms-condition.html">Terms & Condition.</a> </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="button" class="edu-btn btn-medium">Create Account <i class="icon-4"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <ul class="shape-group">
+                    <li class="shape-1 scene"><img data-depth="2" src="assets/images/about/shape-07.png" alt="Shape"></li>
+                    <li class="shape-2 scene"><img data-depth="-2" src="assets/images/about/shape-13.png" alt="Shape"></li>
+                    <li class="shape-3 scene"><img data-depth="2" src="assets/images/counterup/shape-02.png" alt="Shape"></li>
+                </ul>
+            </div>
+        </section>
+        <!--=====================================-->
+        <!--=        Footer Area Start          =-->
+        <!--=====================================-->
+        <!-- Start Footer Area  -->
+        
---
+++ pages/pricing-table.php
@@ -0,0 +1,125 @@
+
+        <!--=====================================-->
+        <!--=       Breadcrumb Area Start      =-->
+        <!--=====================================-->
+
+
+        <div class="edu-breadcrumb-area">
+            <div class="container">
+                <div class="breadcrumb-inner">
+                    <div class="page-title">
+                        <h1 class="title">Membership Plans</h1>
+                    </div>
+                    <ul class="edu-breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="separator"><i class="icon-angle-right"></i></li>
+                        <li class="breadcrumb-item active" aria-current="page">Pricing</li>
+                    </ul>
+                </div>
+            </div>
+            <ul class="shape-group">
+                <li class="shape-1">
+                    <span></span>
+                </li>
+                <li class="shape-2 scene"><img data-depth="2" src="assets/images/about/shape-13.png" alt="shape"></li>
+                <li class="shape-3 scene"><img data-depth="-2" src="assets/images/about/shape-15.png" alt="shape"></li>
+                <li class="shape-4">
+                    <span></span>
+                </li>
+                <li class="shape-5 scene"><img data-depth="2" src="assets/images/about/shape-07.png" alt="shape"></li>
+            </ul>
+        </div>
+
+        <!--=====================================-->
+        <!--=        Event Area Start         =-->
+        <!--=====================================-->
+        <div class="edu-section-gap">
+            <div class="container">
+                <div class="section-title section-center" data-sal="slide-up" data-sal-duration="800">
+                    <span class="pre-title">Pricing Table</span>
+                    <h2 class="title">Great Membership Plan</h2>
+                    <span class="shape-line"><i class="icon-19"></i></span>
+                </div>
+                <div class="row g-5">
+                    <div class="col-lg-4" data-sal-delay="100" data-sal="slide-up" data-sal-duration="800">
+                        <div class="pricing-table">
+                            <div class="pricing-header">
+                                <h6 class="title">Silver Plan</h6>
+                                <div class="price-wrap">
+                                    <span class="amount">$29.00</span>
+                                    <span class="duration">Per month</span>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet consect adipisicing elit sed. do eilt sed</p>
+                            </div>
+                            <div class="pricing-body">
+                                <ul class="list-item">
+                                    <li><i class="icon-20"></i>Courses included: 1</li>
+                                    <li><i class="icon-20"></i>Individual Course</li>
+                                    <li><i class="icon-20"></i>Course learning checks</li>
+                                    <li class="item-off"><i class="icon-20"></i>Course discussions</li>
+                                    <li class="item-off"><i class="icon-20"></i>Offline learning</li>
+                                </ul>
+                            </div>
+                            <div class="pricing-btn">
+                                <a class="edu-btn btn-border btn-medium" href="#">Select plan<i class="icon-east"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4" data-sal-delay="150" data-sal="slide-up" data-sal-duration="800">
+                        <div class="pricing-table">
+                            <div class="pricing-header">
+                                <h6 class="title">Gold Plan</h6>
+                                <div class="price-wrap">
+                                    <span class="amount">$49.00</span>
+                                    <span class="duration">Per month</span>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet consect adipisicing elit sed. do eilt sed</p>
+                            </div>
+                            <div class="pricing-body">
+                                <ul class="list-item">
+                                    <li><i class="icon-20"></i>Courses included: 1</li>
+                                    <li><i class="icon-20"></i>Individual Course</li>
+                                    <li><i class="icon-20"></i>Course learning checks</li>
+                                    <li><i class="icon-20"></i>Course discussions</li>
+                                    <li class="item-off"><i class="icon-20"></i>Offline learning</li>
+                                </ul>
+                            </div>
+                            <div class="pricing-btn">
+                                <a class="edu-btn btn-border btn-medium active" href="#">Select plan<i class="icon-east"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4" data-sal-delay="200" data-sal="slide-up" data-sal-duration="800">
+                        <div class="pricing-table">
+                            <div class="pricing-header">
+                                <h6 class="title">Diamond Plan</h6>
+                                <div class="price-wrap">
+                                    <span class="amount">$79.00</span>
+                                    <span class="duration">Per month</span>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet consect adipisicing elit sed. do eilt sed</p>
+                            </div>
+                            <div class="pricing-body">
+                                <ul class="list-item">
+                                    <li><i class="icon-20"></i>Courses included: 1</li>
+                                    <li><i class="icon-20"></i>Individual Course</li>
+                                    <li><i class="icon-20"></i>Course learning checks</li>
+                                    <li><i class="icon-20"></i>Course discussions</li>
+                                    <li><i class="icon-20"></i>Offline learning</li>
+                                </ul>
+                            </div>
+                            <div class="pricing-btn">
+                                <a class="edu-btn btn-border btn-medium" href="#">Select plan<i class="icon-east"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--=====================================-->
+        <!--=        Footer Area Start          =-->
+        <!--=====================================-->
+        <!-- Start Footer Area  -->
+        
