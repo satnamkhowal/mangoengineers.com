ae2437b57c3003fefe1893f286a5f63e2d4774ee
Satnam Singh <singhkhowal@gmail.com>
2026-09-21 18:32:57 +0530

    Extract PHP page bodies batch 2

---
+++ pages/coming-soon.php
@@ -0,0 +1,31 @@
+
+
+        <!--=====================================-->
+        <!--=     Comming Soon Area Start       =-->
+        <!--=====================================-->
+        <section class="coming-soon-page-area ">
+            <div class="container">
+                <div class="coming-soon-content">
+                    <h1 class="title">Coming Soon</h1>
+                    <div class="coming-countdown"></div>
+                    <div class="input-group">
+                        <input type="email" class="form-control" placeholder="Enter your email address">
+                        <button class="edu-btn btn-medium" type="button">Subscribe <i class="icon-4"></i></button>
+                    </div>
+                </div>
+            </div>
+            <ul class="shape-group">
+                <li class="shape-1 scene">
+                    <img data-depth="2" src="assets/images/others/shape-22.png" alt="Shape">
+                </li>
+                <li class="shape-2 scene">
+                    <img data-depth="-2" src="assets/images/others/shape-23.png" alt="Shape">
+                </li>
+                <li class="shape-3">
+                    <img class="rotateit" src="assets/images/others/shape-24.png" alt="Shape">
+                </li>
+                <li class="shape-4"></li>
+            </ul>
+        </section>
+
+    
