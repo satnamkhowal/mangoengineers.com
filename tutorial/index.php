b4337c94da524e256ba1a22587d1e621c60dca07
Satnam Singh <singhkhowal@gmail.com>
2026-09-22 21:01:57 +0530

    ME-TUTORIAL-001 align renderer paths for CI lint

--- tutorial/index.php
+++ tutorial/index.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/includes/template.php';
-require_once __DIR__ . '/data/tutorials.php';
+require_once __DIR__ . '/template.php';
+require_once __DIR__ . '/tutorials.php';
 
 $tracks = mango_tutorial_tracks();
 $planned = mango_tutorial_planned_tracks();
